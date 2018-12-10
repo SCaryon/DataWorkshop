55ee71c30ffd33d39e6abbfa91ce868255b2c243
weinixiong <669075469@qq.com>
2018-12-10 17:17:39 +0800

    examples/table/User_cluster.py

--- examples/table/User_cluster.py
+++ examples/table/User_cluster.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
-global labels
-table_data, table_features, table_identifiers = read_table_data('examples/table/car.csv')
+if os.path.exists("./static/user/" + session.get('email') + "/data/table.csv"):
+    table_data, table_features, table_identifiers = read_table_data(
+        "./static/user/" + session.get('email') + "/data/table.csv")
+else:
+    table_data, table_features, table_identifiers = read_table_data('./examples/table/car.csv')
 data = np.array(table_data)
 data = preprocessing.MinMaxScaler().fit_transform(data)
 model = KMeans(n_clusters=6)
