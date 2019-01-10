ac336272195f7986c976bfdbb973d9173b41d97f
SeungWook Kim <ksw29zz@gmail.com>
2019-01-10 14:26:15 +0900

    bug edit / overlap url

--- api/new.py
+++ api/new.py
@@ -1,24 +1,37 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, session
+from model import user
 
 
 new = Blueprint('new',__name__,template_folder='../templates_new')
 @new.route("/new/")
 def home():
-    return render_template('front.html')
 
-@new.route("/login/")
+    session['cluster_method'] = 'KMeans'
+    session['embedding_method'] = 'Principal_Component_Analysis'
+    session['visualization_method'] = 'Radviz'
+    session['cluster_parameters'] = {}
+    if session.get('email'):
+        email = session.get('email')
+        user1 = user.query.filter_by(email=email).first()
+        print(user1)
+        return render_template('front.html', user=user1)
+    else:
+        return render_template('front.html')
+
+
+@new.route("/new/login/")
 def login():
     return render_template('login.html')
 
-@new.route("/signup/")
+@new.route("/new/signup/")
 def signup():
     return render_template('signup.html')
 
-@new.route("/term1/")
+@new.route("/new/term1/")
 def term1():
     return render_template('term1.html')
 
-@new.route("/term2")
+@new.route("/new/term2")
 def term2():
     return render_template('term2.html')
 
