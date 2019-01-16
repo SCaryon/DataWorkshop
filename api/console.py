18e0680722b28262b622349c3a0f2b35fc7592b2
LHongze <Lhongze@hotmail.com>
2019-01-16 18:50:52 +0800

    console/text

--- api/console.py
+++ api/console.py
@@ -1,11 +1,25 @@
-from flask import Blueprint, render_template, session,request
+from flask import Blueprint, render_template, session,request, json,jsonify
 from model import user
 import pandas as pd
 import os
 from table_data_io import read_table_data,generate_table_data,data_list_to_dictionary,check_table_data,generate_table_dic_data
 import numpy as np
 from graph_data_io import read_graph_data, check_graph_data
+import jieba
+import wav2text  # wav转text的自定义py文件
+from docx import Document
+from werkzeug.utils import secure_filename
+from aip import AipOcr  # 引入百度api
 
+# 用于执行病毒查杀
+import pyclamd
+# 连接百度服务器的密钥
+APP_ID = '14658891'
+API_KEY = 'zWn97gcDqF9MiFIDOeKVWl04'
+SECRET_KEY = 'EEGvCjpzTtWRO3GIxqz94NLz99YSBIT9'
+# 连接百度服务器
+# 输入三个密钥，返回服务器对象
+client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 url ='console'
 
 console = Blueprint('console',__name__,template_folder='../templates_new')
@@ -111,8 +125,8 @@ def multigoo_visualization():
                            visualization_method=table_visualization_method)
 
 # Product - Text Goo
-@console.route("/"+url+"/textgoo", methods=['GET', 'POST'])
-def textgoo():
+@console.route("/"+url+"/textgoo_knowledgegraph", methods=['GET', 'POST'])
+def textgoo_knowledgegraph():
     if not session.get('email'):
         user1 = None
         path = './examples/text/text_data.csv'
@@ -135,7 +149,120 @@ def textgoo():
             list_num.append(str)
         my_dic = data_list_to_dictionary(features_list, list_num)
         text_no_identifiers_data_dictionary.append(my_dic)
-    return render_template('console/text/textgoo.html', user=is_login(), text_data=text_no_identifiers_data_dictionary)
+    return render_template('console/text/textgoo_knowledgegraph.html', user=is_login(), text_data=text_no_identifiers_data_dictionary)
+
+@console.route("/"+url+"/textgoo_wordcloud")
+def textgoo_wordcloud():
+    return render_template('console/text/textgoo_wordcloud.html', user=is_login())
+
+@console.route('/word_cloud_OCR', methods=['GET', 'POST'])
+def picture_OCR():
+    if session.get('email') and request.method == 'POST':
+        type = request.form.get('label')
+        if type == 'wav':
+            f = request.files['wav']
+            base_path = os.path.dirname(
+                __file__ + '/static/user/' + session.get('email') + "/img")  # 当前文件所在路径
+            upload_path = os.path.join(base_path, '', secure_filename('doc_by_upload.wav'))
+            f.save(upload_path)
+            result = wav2text.wav2word(upload_path)
+            result = json.loads(result)
+            if result["err_msg"] != "success.":
+                os.remove(upload_path)
+                return "err!:" + result["err_msg"]
+            else:
+                os.remove(upload_path)
+                return (" ".join(jieba.cut("".join(result["result"]))))
+        if type == 'image':
+            f = request.files['image']
+            base_path = os.path.dirname(
+                __file__)  # os.path.dirname(__file__) + '/static/user/' + session.get('email') + "/img"# 当前文件所在路径
+            upload_path = os.path.join(base_path, '', secure_filename('image_by_upload.jpg'))
+            f.save(upload_path)
+            # 读取刚储存的本地文件
+            with open(upload_path, 'rb') as fp:
+                image = fp.read()
+            # 输入刚读取的本地文件，调用百度文字识别，返回json格式识别结构
+            result = client.accurate(image)
+
+            # 将百度返回的分行结果连接成一行
+            raw = ""
+            for sresult in result["words_result"]:
+                raw += sresult["words"]
+
+            # 输入连续的文字，返回分词结果
+            x = (" ".join(jieba.cut(raw)))
+
+            # 向浏览器返回分次结果
+            os.remove(upload_path)
+            return x
+        if type == 'docx':
+            f = request.files['docx']
+            base_path = os.path.dirname(
+                __file__)  # os.path.dirname(__file__) + '/static/user/' + session.get('email') + "/img"# 当前文件所在路径
+            upload_path = os.path.join(base_path, '', secure_filename('doc_by_upload.docx'))
+            f.save(upload_path)
+            raw = ""
+            document = Document(upload_path)
+            for paragraph in document.paragraphs:
+                raw += paragraph.text
+                # 输入连续的文字，返回分词结果
+            x = (" ".join(jieba.cut(raw)))
+            # 向浏览器返回分次结果
+            os.remove(upload_path)
+            return x
+        if type == 'text':
+            raw = request.form.get('text')
+            x = (" ".join(jieba.cut(raw)))
+            # 向浏览器返回分次结果
+            return x
+        else:
+            return "we don't support this type of file!"
+    else:
+        return jsonify(False)
+
+
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+
+@console.route('/upload_pic', methods=['POST', 'GET'])
+def upload_pic():
+    # text=pytesseract.image_to_string(Image.open('show.jpg'),lang='chi_sim') #设置为中文文字的识别
+    if session.get('email') and request.method == 'POST':
+        f = request.files['uploadImage']
+        filename = f.filename
+        base_path = "./static/user/" + session.get('email') + "/data"  # 当前文件所在路径
+        upload_path = os.path.join(base_path, '', secure_filename(filename))
+
+        if os.path.exists(upload_path):
+            os.remove(upload_path)
+        f.save(upload_path)  # appends upload.filename automatically
+
+        # 读取刚储存的本地文件
+        image = get_file_content(upload_path)
+
+        # 输入刚读取的本地文件，调用百度文字识别，返回json格式识别结构
+        result = client.basicAccurate(image)
+
+        # 将百度返回的分行结果连接成一行
+        raw = ""
+        for sresult in result["words_result"]:
+            raw += sresult["words"]
+
+        # 输入连续的文字，返回分词结果
+        x = (" ".join(jieba.cut(raw)))
+
+        # 打印分词结果
+        print(x)
+
+        # 向浏览器返回分次结果
+        return x
+    else:
+        session['last_page'] = '/textgoo'
+        return jsonify(False)
+
 
 
 # Product - Geo Goo
